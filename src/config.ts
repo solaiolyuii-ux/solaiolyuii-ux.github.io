6e5f9dd89820882f54c1318bd6aa33632c158823
solaiolyuii-ux <solaiolyuii@gmail.com>
2026-03-09 19:12:21 +0800

    feat: 切换 Twikoo 后端到 Netlify

--- src/config.ts
+++ src/config.ts
@@ -403,7 +403,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo-comments-silk-omega.vercel.app",
+		envId: "https://peaceful-cocada-4a0401.netlify.app/.netlify/functions/twikoo",
 		lang: SITE_LANG,
 	},
 };
